dcc6bf8645cbae414d7c8e0e9d90f6abdaa447dc
isaacbaldridge <baldridgela@alumni.vcu>
2023-11-29 21:39:37 -0500

    up and down functionality for both paddles

--- pong.py
+++ pong.py
@@ -27,10 +27,45 @@ paddle_b.goto(350, 0)
 
 
 # Ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0, 0)
 
+# Functions
+def paddle_a_up():
+    # ycore() returns the y coordinate
+    y = paddle_a.ycor()
+    y += 20
+    paddle_a.sety(y)
 
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -= 20
+    paddle_a.sety(y)
+    
+def paddle_b_up():
+    # ycore() returns the y coordinate
+    y = paddle_b.ycor()
+    y += 20
+    paddle_b.sety(y)
 
-
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -= 20
+    paddle_b.sety(y)
+    
+        
+# Keyboard binding
+wn.listen()
+wn.onkeypress(paddle_a_up, "w")
+wn.onkeypress(paddle_a_down, "s")
+wn.onkeypress(paddle_b_up, "Up")
+wn.onkeypress(paddle_b_down, "Down")
+    
+    
 # Main Game Loop
 
 while True:
