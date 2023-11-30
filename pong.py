bf9a09e8fa98c96d1645411b82ce1db8f0e212b9
isaacbaldridge <baldridgela@alumni.vcu>
2023-11-29 21:56:52 -0500

    added border collision to ball

--- pong.py
+++ pong.py
@@ -34,7 +34,7 @@ ball.color("white")
 ball.penup()
 ball.goto(0, 0)
 ball.dx = 0.1
-ball.dy = 0.1
+ball.dy = -0.1
 
 
 # Functions
@@ -80,4 +80,18 @@ while True:
     
     
     # Border checking
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+        
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
     
+    if ball.xcor() > 390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+        
+    if ball.xcor() < -390:
+        ball.goto(0, 0)
+        ball.dx *= -1
