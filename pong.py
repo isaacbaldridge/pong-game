8709b0fc907407a806981a2bc1a75cb8dd49fafd
isaacbaldridge <baldridgela@alumni.vcu>
2023-11-29 22:39:15 -0500

    added gradual speed increase each time a paddle hits the ball

--- pong.py
+++ pong.py
@@ -7,6 +7,10 @@ wn.bgcolor("black")
 wn.setup(width = 800, height = 600)
 wn.tracer(0)
 
+# Score
+score_a = 0
+score_b = 0
+
 # Paddle A
 paddle_a = turtle.Turtle()
 paddle_a.speed(0)
@@ -36,6 +40,14 @@ ball.goto(0, 0)
 ball.dx = 0.1
 ball.dy = -0.1
 
+# Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("P1: 0                                    P2: 0", align = "center", font = ("Courier", 20, "normal"))
 
 # Functions
 def paddle_a_up():
@@ -90,17 +102,27 @@ while True:
     
     if ball.xcor() > 390:
         ball.goto(0, 0)
-        ball.dx *= -1
+        ball.dx = -0.1
+        score_a += 1
+        pen.clear()
+        pen.write("P1: {}                                    P2: {}".format(score_a, score_b), align = "center", font = ("Courier", 20, "normal"))
+        
         
     if ball.xcor() < -390:
         ball.goto(0, 0)
-        ball.dx *= -1
+        ball.dx = 0.1
+        score_b += 1
+        pen.clear()
+        pen.write("P1: {}                                    P2: {}".format(score_a, score_b), align = "center", font = ("Courier", 20, "normal"))
+        
         
     # Paddle and ball collisions
-    if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() - 40):
+    if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 49 and ball.ycor() > paddle_b.ycor() - 49):
         ball.setx(340)
-        ball.dx *= -1
+        ball.dx *= -1.15
         
-    if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() - 40):
+    if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 49 and ball.ycor() > paddle_a.ycor() - 49):
         ball.setx(-340)
-        ball.dx *= -1
+        ball.dx *= -1.15
+        
+    
